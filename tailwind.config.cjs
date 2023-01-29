6ff772d9e9b4d1f8e8da39fa4c52b919c6d73a82
ErnestasSku <ernestassku@gmail.com>
2023-01-30 00:36:16 +0200

    package changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,10 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,svelte}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
