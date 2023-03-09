6e71084dd983d476564d2696ea32816459d635fb
ErnestasSku <ernestassku@gmail.com>
2023-03-09 21:16:26 +0200

    Add custom daisy UI theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,svelte}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,svelte}"],
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#36D399",
+
+          secondary: "#f472b6",
+
+          accent: "#22d3ee",
+
+          neutral: "#191D24",
+
+          "base-100": "#2A303C",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
-}
+};
