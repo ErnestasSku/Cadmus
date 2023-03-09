ef64bafbd11c40f4c80f872b0f17e7599b5bf8d0
ErnestasSku <ernestassku@gmail.com>
2023-03-09 21:16:46 +0200

    Create connection input component

---
+++ src/lib/ConnectionInput.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  export let conn;
+</script>
+
+<main class="mt-1">
+  <div>
+    <input class="input" />
+    <vl />
+    <input class="input" />
+    <vl />
+    <span class="dot" />
+  </div>
+</main>
+
+<style>
+
+  div {
+    display: flex;
+    justify-content: center;
+  }
+
+  input {
+    width: 48%;
+  }
+
+  input:focus,
+  textarea:focus {
+    outline: 2px solid #f472b6;
+    outline-offset: 2px;
+  }
+
+  vl {
+    width: 2%;
+  }
+
+  .dot {
+    height: 25px;
+    width: 25px;
+    background-color: #22d3ee;
+    border-radius: 50%;
+    margin-top: 0.5em;
+  }
+
+  .dot:hover {
+    @apply scale-100 hover:scale-125 duration-300 ease-linear transition-all;
+  }
+
+</style>
