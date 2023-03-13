f25313d8a254a01a3c5ee73c2a2405ec0e26fa83
ErnestasSku <ernestassku@gmail.com>
2023-03-13 20:44:40 +0200

    Add interfaces

---
+++ src/typescript/interfaces.ts
@@ -0,0 +1,19 @@
+export interface StoryBlock {
+  index: number;
+  top: number;
+  left: number;
+  connections: Connection[];
+}
+
+export interface Connection {
+  index: number;
+  pathLabel: string;
+  pathDescription: string;
+  startX: number;
+  startY: number;
+  endX: number;
+  endY: number;
+  empty: boolean;
+  connected: boolean;
+  visible: boolean;
+}
